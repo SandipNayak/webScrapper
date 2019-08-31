3fc0d04abf1db4a0a9d453cb79f3dc6114411f20
Jyoti Jayadeep <44501265+obli99@users.noreply.github.com>
2019-08-31 19:06:05 +0530

    search code added

--- a1.py
+++ a1.py
@@ -1,4 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-val=requests.get('')
+headers={"User-Agent" : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+text=requests.get('https://www.amazon.in/s?k=mobile&ref=nb_sb_noss_2', headers=headers)
+
+soup = BeautifulSoup(text.content, 'html.parser')
+tittle = soup.findAll("span" , {"class" : 'a-size-medium a-color-base a-text-normal'})
+price = soup.findAll("span" , {"class" : 'a-price-whole'})
+
+num = len(tittle)
+
+for i in range(num):
+        print("-----------------------Item----------------------")
+        print(tittle[i].text.strip())
+        print("-----------------------Price---------------------")
+        print(price[i].text)
